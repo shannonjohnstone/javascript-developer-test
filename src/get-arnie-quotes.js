f2fadc934745da95d0c86d97f30a053ffaebebc9
Shannon Johnstone <shannon@shannonjohnstone.com.au>
2021-05-18 12:05:35 +1000

    Implement first pass of getArnieQuotes, all tests passing

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,18 @@
 const { httpGet } = require('./mock-http-interface');
 
+async function getQuote(url) {
+  try {
+    const { status, body } = await httpGet(url);
+    const message = JSON.parse(body).message
+    return status === 200 ? { "Arnie Quote": message } : { FAILURE: message }
+  } catch (error) {
+    return { FAILURE: error.message }
+  }
+}
+
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  const response = urls.map(getQuote)
+  return Promise.all(response)
 };
 
 module.exports = {
